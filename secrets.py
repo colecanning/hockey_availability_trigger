301c8a7a40e3566ba83d11236ea9a21a09968f6a
Cole Canning <ccanning10@gmail.com>
2020-03-08 17:14:18 -0400

    stuff

--- secrets.py
+++ secrets.py
@@ -9,32 +9,32 @@ def get_secret(project_id, secret_id):
     the secret container, not any secret material.
     """
 
-    # Import the Secret Manager client library.
-    from google.cloud import secretmanager_v1beta1 as secretmanager
-
     # Create the Secret Manager client.
     client = secretmanager.SecretManagerServiceClient()
 
     # Build the resource name of the secret.
     name = client.secret_path(project_id, secret_id)
+    print(name)
+
 
     # Get the secret.
     response = client.get_secret(name)
+    exit()
 
-    # Get the replication policy.
-    if response.replication.automatic:
-        replication = 'AUTOMATIC'
-    elif response.replication.user_managed:
-        replication = 'MANAGED'
-    else:
-        raise 'Unknown replication {}'.format(response.replication)
+#     # Get the replication policy.
+#     if response.replication.automatic:
+#         replication = 'AUTOMATIC'
+#     elif response.replication.user_managed:
+#         replication = 'MANAGED'
+#     else:
+#         raise 'Unknown replication {}'.format(response.replication)
 
-    # Print data about the secret.
-    print('Got secret {} with replication policy {}'.format(
-        response.name, replication))
-# [END secretmanager_get_secret]
+#     # Print data about the secret.
+#     print('Got secret {} with replication policy {}'.format(
+#         response.name, replication))
+# # [END secretmanager_get_secret]
 
-    return response
+#     return response
 
 
 print(get_secret("780728492658", "TestSecret"))
