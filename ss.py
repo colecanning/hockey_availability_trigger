b9e3f260ebeb1f4317274b9dd8a3a020b4b7ff87
Cole Canning <ccanning10@gmail.com>
2020-03-08 20:31:37 -0400

    asdf

---
+++ ss.py
@@ -0,0 +1,7 @@
+secrets = secretmanager.SecretManagerServiceClient()
+projectid = "587789046371"
+
+
+ALPHA_VANTAGE_KEY = secrets.access_secret_version("projects/"+projectid+"/secrets/alpha-vantage-key/versions/1").payload.data.decode("utf-8")
+
+print(ALPHA_VANTAGE_KEY)
